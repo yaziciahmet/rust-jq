14de0035083848e3da057f00c89c255d367dda15
yaziciahmet <yaziciahmet@sabanciuniv.edu>
2024-04-12 18:28:57 +0200

    Error handling in Tokenizer & unit tests

--- src/json/token.rs
+++ src/json/token.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Debug, Display};
+
 #[derive(Debug, PartialEq)]
 pub enum Token {
     BraceOpen,
@@ -16,19 +18,28 @@ pub enum Token {
 pub struct Tokenizer<'a> {
     contents: &'a str,
     pos: usize,
+    error: Option<TokenError>,
 }
 
 impl<'a> Tokenizer<'a> {
     pub fn new(contents: &str) -> Tokenizer {
-        Tokenizer { contents, pos: 0 }
+        Tokenizer {
+            contents,
+            pos: 0,
+            error: None,
+        }
+    }
+
+    fn peek_nth_char(&self, n: usize) -> Option<char> {
+        self.contents.chars().nth(n)
     }
 
     fn peek_char(&self) -> Option<char> {
-        self.contents.chars().nth(self.pos)
+        self.peek_nth_char(self.pos)
     }
 
     fn next_char(&mut self) -> Option<char> {
-        self.contents.chars().nth(self.pos).map(|c| {
+        self.peek_char().map(|c| {
             self.pos += 1;
             c
         })
@@ -53,62 +64,131 @@ impl<'a> Tokenizer<'a> {
                 'f' => self.read_bool_false(),
                 'n' => self.read_null(),
                 '0'..='9' | '-' => self.read_number(c),
-                _ => None,
+                _ => {
+                    self.set_error();
+                    None
+                }
             };
         }
     }
 
     fn read_string(&mut self) -> Option<Token> {
         let mut s = String::new();
+        let mut peek_pos = self.pos;
         loop {
-            match self.next_char()? {
-                '"' => break,
-                c => s.push(c),
+            let c = self.peek_nth_char(peek_pos);
+            match c {
+                Some('\n') | Some('\r') | None => {
+                    self.set_error();
+                    return None;
+                }
+                Some('"') => break,
+                Some(c) => {
+                    s.push(c);
+                    peek_pos += 1;
+                }
             }
         }
+        self.pos = peek_pos + 1;
         Some(Token::String(s))
     }
 
     fn read_bool_true(&mut self) -> Option<Token> {
-        if self.next_char()? == 'r' && self.next_char()? == 'u' && self.next_char()? == 'e' {
+        if self.peek_char() == Some('r')
+            && self.peek_nth_char(self.pos + 1) == Some('u')
+            && self.peek_nth_char(self.pos + 2) == Some('e')
+        {
+            self.pos += 3;
             Some(Token::True)
         } else {
+            self.set_error();
             None
         }
     }
 
     fn read_bool_false(&mut self) -> Option<Token> {
-        if self.next_char()? == 'a'
-            && self.next_char()? == 'l'
-            && self.next_char()? == 's'
-            && self.next_char()? == 'e'
+        if self.peek_char() == Some('a')
+            && self.peek_nth_char(self.pos + 1) == Some('l')
+            && self.peek_nth_char(self.pos + 2) == Some('s')
+            && self.peek_nth_char(self.pos + 3) == Some('e')
         {
+            self.pos += 4;
             Some(Token::False)
         } else {
+            self.set_error();
             None
         }
     }
 
     fn read_null(&mut self) -> Option<Token> {
-        if self.next_char()? == 'u' && self.next_char()? == 'l' && self.next_char()? == 'l' {
+        if self.peek_char() == Some('u')
+            && self.peek_nth_char(self.pos + 1) == Some('l')
+            && self.peek_nth_char(self.pos + 2) == Some('l')
+        {
+            self.pos += 3;
             Some(Token::Null)
         } else {
+            self.set_error();
             None
         }
     }
 
     fn read_number(&mut self, first: char) -> Option<Token> {
         let mut s = first.to_string();
+        let mut peek_pos = self.pos;
         loop {
-            match self.peek_char()? {
-                '0'..='9' | '.' | 'e' | 'E' | '+' | '-' => {
-                    s.push(self.next_char()?);
-                }
-                _ => break,
+            let c = self.peek_nth_char(peek_pos);
+            match c {
+                Some(c) => match c {
+                    '0'..='9' | '.' | 'e' | 'E' | '+' | '-' => {
+                        s.push(c);
+                        peek_pos += 1;
+                    }
+                    _ => break,
+                },
+                None => break,
             }
         }
-        let n = s.parse().ok()?;
-        Some(Token::Number(n))
+        // rust parser allows trailing dot, but it is invalid JSON
+        if s.ends_with('.') {
+            self.set_error();
+            return None;
+        }
+        // rust parser allows prefix zero (e.g. 01), but it is invalid JSON
+        if s.len() > 1 && s.starts_with('0') && !s.starts_with("0.") && !s.starts_with("0e") {
+            self.set_error();
+            return None;
+        }
+
+        if let Ok(n) = s.parse() {
+            self.pos = peek_pos;
+            Some(Token::Number(n))
+        } else {
+            self.set_error();
+            None
+        }
+    }
+
+    pub fn try_collect(mut self) -> Result<Vec<Token>, TokenError> {
+        let mut tokens = Vec::new();
+        loop {
+            match self.next_token() {
+                Some(token) => {
+                    tokens.push(token);
+                }
+                None => break,
+            }
+        }
+
+        if let Some(error) = self.error {
+            Err(error)
+        } else {
+            Ok(tokens)
+        }
+    }
+
+    fn set_error(&mut self) {
+        self.error = Some(TokenError(self.pos - 1));
     }
 }
 
@@ -117,6 +197,15 @@ impl<'a> Iterator for Tokenizer<'a> {
 
     fn next(&mut self) -> Option<Self::Item> {
         self.next_token()
+    }
+}
+
+#[derive(Debug)]
+pub struct TokenError(usize);
+
+impl Display for TokenError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Unexpected token at position {}", self.0)
     }
 }
 
@@ -137,7 +226,7 @@ mod tokenizer {
         "#;
 
         let tokenizer = Tokenizer::new(contents);
-        let tokens: Vec<Token> = tokenizer.collect();
+        let tokens = tokenizer.try_collect().expect("Failed to collect tokens");
         assert_eq!(
             tokens,
             vec![
@@ -170,5 +259,96 @@ mod tokenizer {
                 Token::BraceClose,
             ]
         );
+    }
+
+    #[test]
+    fn test_valid_tokens_invalid_json() {
+        let contents = r#"
+            {
+                "key", "value", [], {, 123 true
+            }
+        "#;
+        let tokenizer = Tokenizer::new(contents);
+        let tokens = tokenizer.try_collect().expect("Expected error");
+        assert_eq!(
+            tokens,
+            vec![
+                Token::BraceOpen,
+                Token::String("key".to_string()),
+                Token::Comma,
+                Token::String("value".to_string()),
+                Token::Comma,
+                Token::BracketOpen,
+                Token::BracketClose,
+                Token::Comma,
+                Token::BraceOpen,
+                Token::Comma,
+                Token::Number(123.0),
+                Token::True,
+                Token::BraceClose,
+            ]
+        );
+    }
+
+    #[test]
+    fn test_invalid_token_in_json() {
+        let contents = r#"
+            {
+                "key": "value",
+                "number": 42,
+                "bool": true,
+                "null": null,
+                "array": [1, 2, 3],
+                extra
+            }
+        "#;
+        let tokenizer = Tokenizer::new(contents);
+        let err = tokenizer.try_collect().expect_err("Expected error");
+        assert_eq!(err.0, contents.find("extra").unwrap());
+    }
+
+    #[test]
+    fn test_invalid_bool() {
+        let contents = "tru";
+        let tokenizer = Tokenizer::new(contents);
+        let err = tokenizer.try_collect().expect_err("Expected error");
+        assert_eq!(err.0, 0);
+
+        let contents = "fals";
+        let tokenizer = Tokenizer::new(contents);
+        let err = tokenizer.try_collect().expect_err("Expected error");
+        assert_eq!(err.0, 0);
+    }
+
+    #[test]
+    fn test_invalid_str() {
+        let contents = r#""abc"#;
+        let tokenizer = Tokenizer::new(contents);
+        let err = tokenizer.try_collect().expect_err("Expected error");
+        assert_eq!(err.0, 0);
+
+        let contents = r#""abc
+        ""#;
+        let tokenizer = Tokenizer::new(contents);
+        let err = tokenizer.try_collect().expect_err("Expected error");
+        assert_eq!(err.0, 0);
+    }
+
+    #[test]
+    fn test_invalid_number() {
+        let contents = "1.";
+        let tokenizer = Tokenizer::new(contents);
+        let err = tokenizer.try_collect().expect_err("Expected error");
+        assert_eq!(err.0, 0);
+
+        let contents = "1.1e";
+        let tokenizer = Tokenizer::new(contents);
+        let err = tokenizer.try_collect().expect_err("Expected error");
+        assert_eq!(err.0, 0);
+
+        let contents = "01";
+        let tokenizer = Tokenizer::new(contents);
+        let err = tokenizer.try_collect().expect_err("Expected error");
+        assert_eq!(err.0, 0);
     }
 }
